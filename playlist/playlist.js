e8c08decd84f8f0dbad439f33d796aa5ca18e2d7
Derick Montague <derick@beyondhyper.com>
2013-09-20 23:13:07 -0500

    Removed json playlist file. Added xml playlist

--- playlist/playlist.js
+++
@@ -1,9 +0,0 @@
-var playlist = {
-	track : {
-		title: 'Waving Not Drowning',
-		artist: 'Orbital',
-		mp3: '../mp3/Orbital-The Altogether-WavingNotDrowning.mp3',
-		free: 'false',
-		duration: '4:31'
-	}
-}

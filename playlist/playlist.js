b73fcbab173a8d2fa1cea3558f3d05e4592473cb
Derick Montague <derick@beyondhyper.com>
2013-09-20 23:03:34 -0500

    First commit

---
+++ playlist/playlist.js
@@ -0,0 +1,9 @@
+var playlist = {
+	track : {
+		title: 'Waving Not Drowning',
+		artist: 'Orbital',
+		mp3: '../mp3/Orbital-The Altogether-WavingNotDrowning.mp3',
+		free: 'false',
+		duration: '4:31'
+	}
+}
